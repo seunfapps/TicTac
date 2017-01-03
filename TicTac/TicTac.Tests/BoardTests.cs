9cffc388522da6904be98c842258aec0ba18d48d
seunfapps <seunfapps@gmail.com>
2017-01-03 15:12:49 +0100

    Structuring and Tests

---
+++ TicTac/TicTac.Tests/BoardTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TicTac;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TicTac.Tests {
+    [TestClass]
+    public class BoardTests {
+
+        [TestMethod]
+        public void testThatWidthAndHeightAreSame() {
+            var board = new Board(3);
+            Assert.AreEqual(board.Count, board[0].Count);
+        }
+
+        [TestMethod]
+        public void testBoardIsEmpty()
+        {
+            string jsonData = System.IO.File.ReadAllText("Data/empty-board.json");
+            Assert.IsNotNull(jsonData);
+            Assert.AreNotEqual(jsonData, String.Empty);
+            Board board = JsonConvert.DeserializeObject<Board>(jsonData);
+            Assert.IsTrue(board.isEmpty());
+            Assert.IsTrue(board.isValid());
+        }
+    }
+}
